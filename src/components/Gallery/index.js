30aeb38a91c1905e7251e0703f29d3e2b5650334
Philip Schönholzer <phi.sch@hotmail.ch>
2019-01-13 20:30:29 +0100

    Lightbox for gallery images

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 
 import './style.sass'
@@ -7,19 +7,42 @@ import PreviewCompatibleImage from '../PreviewCompatibleImage'
 const aspectToClass = aspectRatio =>
   aspectRatio > 1.5 ? 'landscape' : aspectRatio < 0.8 ? 'portrait' : 'square'
 
-const Gallery = ({ images }) => (
-  <div className="gallery">
-    {images.map(({ image, text }) => (
-      <div
-        className={`item ${aspectToClass(
-          image.childImageSharp.fluid.aspectRatio,
-        )}`}
-      >
-        <PreviewCompatibleImage imageInfo={{ image, alt: text }} />
+const Gallery = ({ images }) => {
+  const [open, setOpen] = useState(false)
+  const [index, setIndex] = useState(0)
+  return (
+    <>
+      <div className="gallery">
+        {images.map(({ image, text }, i) => (
+          <div
+            className={`item ${aspectToClass(
+              image.childImageSharp.fluid.aspectRatio,
+            )}`}
+            onClick={() => {
+              setOpen(true)
+              setIndex(i)
+            }}
+          >
+            <PreviewCompatibleImage imageInfo={{ image, alt: text }} />
+          </div>
+        ))}
       </div>
-    ))}
-  </div>
-)
+      {open && (
+        <div className="modal" onClick={() => setOpen(false)}>
+          <img
+            srcSet={images[index].image.childImageSharp.fluid.srcSet}
+            onClick={e => {
+              e.stopPropagation()
+              setIndex(preIndex =>
+                images.length - 1 > preIndex ? preIndex + 1 : preIndex,
+              )
+            }}
+          />
+        </div>
+      )}
+    </>
+  )
+}
 
 Gallery.propTypes = {
   images: PropTypes.arrayOf({
